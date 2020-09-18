7937f04e7972440bf888801a03d2a685ff7696fb
silas jimmy <jimmysilas17@gmail.com>
2020-09-18 09:14:20 +0300

    Added light and dark modes

--- js/index.js
+++ js/index.js
@@ -3,8 +3,12 @@ class Portfolio {
     this.DOM = {};
     this.DOM.el = el;
 
+    this.DOM.elBody = this.DOM.el.parentNode;
     this.DOM.main = this.DOM.el.querySelector('.main');
     this.DOM.header = this.DOM.main.querySelector('header');
+    this.DOM.dropdown = this.DOM.header.querySelector('.dropdown');
+    this.DOM.dropdownTrigger = this.DOM.dropdown.querySelector('.dropdown_trigger');
+    this.DOM.dropdownContent = this.DOM.dropdown.querySelector('.dropdown_content');
     this.DOM.sidenavOpen = this.DOM.header.querySelector('.open');
     this.DOM.sidenav = this.DOM.el.querySelector('.sidenav');
     this.DOM.sidenavClose = this.DOM.sidenav.querySelector('.close');
@@ -19,7 +23,7 @@ class Portfolio {
     this.DOM.slides[this.current].classList.add('active--slide', 'fade--in');
     this.DOM.indicators[this.current].classList.add('active--indicator');
 
-    // console.dir(this.DOM.sidenav);
+    // console.log(this.DOM.elBody);
 
     this.initEvents();
   }
@@ -35,16 +39,27 @@ class Portfolio {
 
     this.DOM.sidenav.addEventListener('click', (e) => {
       if (e.target.id != "sidenav__link") return;
-      this.toggleSidenav();
+      this.DOM.sidenav.classList.toggle('sidenav--open')
     })
 
     this.DOM.nextSlideBtn.addEventListener('click', () => this.moveTo("next"));
 
     this.DOM.prevSlideBtn.addEventListener('click', () => this.moveTo("prev"));
 
-    this.DOM.sidenavOpen.addEventListener('click', () => this.toggleSidenav());
+    this.DOM.sidenavOpen.addEventListener('click', () => this.DOM.sidenav.classList.toggle('sidenav--open'));
 
-    this.DOM.sidenavClose.addEventListener('click', () => this.toggleSidenav());
+    this.DOM.sidenavClose.addEventListener('click', () => this.DOM.sidenav.classList.toggle('sidenav--open'));
+
+    this.DOM.dropdownContent.addEventListener('click', e => {
+      // Get the previous mode
+      let prevMode = this.DOM.elBody.className;
+      // Get the new mode
+      let currentMode = e.target.id === "dark" ? "dark--mode" : "light--mode";
+      // Replace the previous mode with current mode
+      this.DOM.elBody.classList.replace(prevMode, currentMode);
+      // Replace the text in dropdown trigger
+      this.DOM.dropdownTrigger.textContent = e.target.textContent;
+    })
   }
 
   moveTo(direction) {
@@ -55,10 +70,6 @@ class Portfolio {
 
     this.DOM.slides[this.current].classList.add("active--slide", "fade--in");
     this.DOM.indicators[this.current].classList.add('active--indicator');
-  }
-
-  toggleSidenav() {
-    this.DOM.sidenav.classList.toggle('sidenav--open');
   }
 }
 
