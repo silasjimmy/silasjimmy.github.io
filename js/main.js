141bfa355da7da007f8ae101125c6c220bcf00fe
Silas Jimmy <jimmysilas17@gmail.com>
2024-03-01 04:49:58 +0300

    refactor: add page sidenav

--- js/main.js
+++ js/main.js
@@ -9,20 +9,20 @@
 
 // const site = new Site(document.getElementsByTagName("body"));
 
-function debounce(func, delay) {
-  let timer;
-  return function () {
-    const context = this;
-    const args = arguments;
-    clearTimeout(timer);
-    timer = setTimeout(function () {
-      func.apply(context, args);
-    }, delay);
-  };
-}
+// function debounce(func, delay) {
+//   let timer;
+//   return function () {
+//     const context = this;
+//     const args = arguments;
+//     clearTimeout(timer);
+//     timer = setTimeout(function () {
+//       func.apply(context, args);
+//     }, delay);
+//   };
+// }
 
-function animateNavbar() {
-  console.log(document.getElementById("navbar"));
-}
+// function animateNavbar() {
+//   console.log(document.getElementById("navbar"));
+// }
 
-animateNavbar();
+// animateNavbar();
