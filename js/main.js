e5100c0b6108e18420033f8958aab91f9c116337
Silas Jimmy <jimmysilas17@gmail.com>
2025-03-27 01:16:38 +0300

    refactor: update headline, bio, education and footer details

--- js/main.js
+++ js/main.js
@@ -1,24 +1,6 @@
 class Portfolio {
   constructor(el) {
     this.el = el;
-    this.hiddenContent = document.querySelector(".hidden--content");
-    this.toggleBtn = document.getElementById("toggle--hidden");
-
-    this.initEvents();
-  }
-
-  initEvents() {
-    this.toggleBtn.addEventListener("click", () => this.toggleHiddenContent());
-  }
-
-  toggleHiddenContent() {
-    if (this.hiddenContent.classList.contains("hide")) {
-      this.hiddenContent.classList.remove("hide");
-      this.toggleBtn.innerHTML = "show less";
-    } else {
-      this.hiddenContent.classList.add("hide");
-      this.toggleBtn.innerHTML = "show more";
-    }
   }
 }
 
